7d9b8291c3d91a80c53412dd93552beb0fd60e81
Russell Horton <horton@ecohealthalliance.org>
2015-03-23 10:41:41 -0700

    move schema reading and type guessing in tests to server side, factor out logic

--- tests/cucumber/features/step_definitions/report_form_steps.js
+++ tests/cucumber/features/step_definitions/report_form_steps.js
@@ -8,6 +8,8 @@
 
   var _ = Package["underscore"]._;
 
+  var helpers = require('./helpers')
+
   module.exports = function () {
 
     var helper = this;
@@ -19,59 +21,18 @@
         assert(exists);
       })
       .execute(function(){
-        var schema = collections.Reports.simpleSchema()._schema;
-        var types = {};
-        _.each(schema, function (value, key) {
-          // studyId is designated as a select via options passed in via
-          // a helper, so it's not in the schema and can't be detected here.
-          if (key == 'studyId') {
-            types[key] = 'select';
-          }   else if (value.optional) {
-              types[key] = 'optional';
-          } else if (value.autoform &&
-              value.autoform.afFieldInput &&
-              value.autoform.afFieldInput.options &&
-              value.autoform.afFieldInput.noselect) {
-            types[key] = 'radio';
-          } else if (value.autoform &&
-              value.autoform.afFieldInput &&
-              value.autoform.options &&
-              value.autoform.afFieldInput.noselect) {
-            types[key] = 'radio';
-          } else if (value.autoform &&
-              value.autoform.afFieldInput &&
-              value.autoform.afFieldInput.options) {
-            types[key] = 'select';
-          } else if (value.autoform &&
-              value.autoform.afFieldInput &&
-              value.autoform.options) {
-            types[key] = 'select';
-          } else if (value.autoform &&
-                     value.autoform.rows) {
-            types[key] = 'textarea';
-          } else if (value.autoform &&
-                     value.autoform.afFieldValueContains) {
-            types[key] = 'optional';
-          } else {
-            types[key] = value.type.name;
-          }
-        });
-
-        return {
-          formData: AutoForm.Fixtures.getData(collections.Reports.simpleSchema()),
-          schemaTypes: types
-        };
+        return AutoForm.Fixtures.getData(collections.Reports.simpleSchema());
       }, function(err, res){
         if(err) {
           return callback.fail(err);
         } else {
-          var schemaTypes = res.value.schemaTypes;
-          var generatedFormData = res.value.formData;
+          var generatedFormData = res.value
           generatedFormData['studyId'] = 'fakeid';
-          generatedFormData['contact']['name'] = 'Fake Name';
-          generatedFormData['contact']['email'] = 'foo@bar.com';
+          generatedFormData['contact.name'] = 'Fake Name';
+          generatedFormData['contact.email'] = 'foo@bar.com';
           generatedFormData['images'] = [];
           generatedFormData['pathologyReports'] = [];
+          generatedFormData['consent'] = true;
           generatedFormData['eventLocation'] = null;
           if (generatedFormData.hasOwnProperty('eventDate')) {
             generatedFormData['eventDate'] = new Date(
@@ -79,41 +40,13 @@
               .toISOString().slice(0,10);
           }
           _.extend(generatedFormData, customValues);
-          var browser = helper.world.browser;
-          _.each(generatedFormData, function (value, key) {
-            if (schemaTypes[key] === 'String' ||
-                schemaTypes[key] === 'Number') {
-              if (value) {
-                browser.setValue('input[data-schema-key="' + key + '"]', value);
-              }
-            } else if (schemaTypes[key] === 'textarea') {
-              browser.setValue('textarea[data-schema-key="' + key + '"]', value);
-            } else if (schemaTypes[key] === 'Boolean') {
-              browser.click('div[data-schema-key="' + key + '"] input[value="' + value + '"]');
-            } else if (schemaTypes[key] === 'radio') {
-              browser.click('div[data-schema-key="' + key + '"] input[value="' + value + '"]');
-            } else if (schemaTypes[key] === 'select') {
-              browser.selectByValue('select[data-schema-key="' + key + '"]', value);
-            } else if (schemaTypes[key] === 'Array') {
-              _.each(value, function (element) {
-                browser.click('div[data-schema-key="' + key + '"] input[value="' + element + '"]');
-              });
-            } else if (schemaTypes[key] === 'Date') {
-              browser.setValue('input[data-schema-key="' + key + '"]', '');
-            } else if (schemaTypes[key] === 'Object') {
-              _.each(value, function (subValue, subKey) {
-                var schemaKey = key + '.' + subKey;
-                browser.setValue('input[data-schema-key="' + schemaKey + '"]', subValue);
-              });
-            } else if (schemaTypes[key] = 'optional') {
-              // do nothing
-            } else {
-              var error = 'unknown type in schema: ' + schemaTypes[key];
-              throw new Error(error);
-            }
-          });
-          callback();
-        }});
+
+          helpers.setFormFields(collections.Reports.simpleSchema()._schema,
+                                generatedFormData,
+                                helper.world.browser);
+        }
+        callback();
+      });
     };
 
     this.When(/^I fill out the form with the (eventDate) "([^"]*)"$/,
